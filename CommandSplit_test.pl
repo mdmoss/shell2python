c6f2e0c96645b100fc0103775ee92fb5046be38f
Matthew Moss <mdm@cse.unsw.edu.au>
2012-09-01 13:27:43 +1000

    Remove unused CommandSplit module

--- CommandSplit_test.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -w
-use strict;
-
-use CommandSplit;
-use Test::More tests => 3;
-
-is (CommandSplit::to_py_list ("ls"), "['ls']");
-is (CommandSplit::to_py_list ("ls -l"), "['ls', '-l']");
-is (CommandSplit::to_py_list ("ls -l /tmp/"), "['ls', '-l', '/tmp/']");
