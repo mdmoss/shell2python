91a287399cb7dcfe4527ae4d4f974310f38ca9e3
Matt <mdm@cse.unsw.edu.au>
2012-09-10 15:00:50 +1000

    Add test00

---
+++ mdm_tests/test00.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2.7 -u
+
+# This is a test of Level 0 features. Expect the unexpected. # Like that
+# All the cool kids use #as#es instead of the letter H now
+# "It's a thing" they tell me, but who can be sure
+# Holy Syntax Batman !@#$%^&*()|~?":?":?13456780=-=-=\\\
+
+print 'This', 'seems', 'to', 'work'
+word1 = 'Yes'
+word2 = 'does'
+print word1, 'it', word2
