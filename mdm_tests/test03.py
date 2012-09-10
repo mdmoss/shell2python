c9bd70760d72bbd754e68444cc7a4ad9469b3db8
Matt <mdm@cse.unsw.edu.au>
2012-09-10 17:02:26 +1000

    Add test03 with for string things

---
+++ mdm_tests/test03.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2.7 -u
+
+# We're really going to town with quotes today
+
+print 'line', 1
+print 'line 2'
+print "line 3"
+print '"line 4 has quotes"'
+print "'line 5 also has quotes'"
+print "line 6's grammer leaves little to be desired"
+word = 'rockin'
+print "line 7's variable interpolation is " + str(word)
